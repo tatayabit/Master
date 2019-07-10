237974d10e1e1d6099834f7532b6b35ba26a4fe1
Kareem Mohammed <km.tatayab@gmail.com>
2019-07-10 11:27:19 +0300

    1.Fixed ProductResult encoding / decoding Model.

--- Tatayaba/Tatayaba/Modules/Products/Models/Product.swift
+++ Tatayaba/Tatayaba/Modules/Products/Models/Product.swift
@@ -11,8 +11,18 @@ struct ProductsResult {
 }
 
 extension ProductsResult: Codable {
-    enum ProductsResultKeys: String, CodingKey {
+    enum ProductsResultCodingKeys: String, CodingKey {
         case products
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ProductsResultCodingKeys.self)
+        products = try container.decodeIfPresent([Product].self, forKey: .products)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: ProductsResultCodingKeys.self)
+        try container.encode(products, forKey: .products)
     }
 }
 
