2fec0203ccefa103efd39b10602d245119b1e2af
Kareem Mohammed <km.tatayab@gmail.com>
2019-09-10 19:16:42 +0300

    Updating new cart VC in progress.

---
+++ Tatayaba/Tatayaba/Modules/Cart/Views/NewCartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewCartViewController.swift
+//  Tatayaba
+//
+//  Created by Kareem Kareem on 9/10/19.
+//  Copyright © 2019 Shaik. All rights reserved.
+//
+
+import UIKit
+
+class NewCartViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
