1c7e10383febdb79e8f7ff6c333b7d764b7a8254
Kareem Mohammed <km.tatayab@gmail.com>
2019-09-14 12:04:39 +0300

    1. Added Concierge image picker selection. 2. Added image picker class.

--- Tatayaba/Tatayaba/Modules/Concierge/Views/ConciergeViewController.swift
+++ Tatayaba/Tatayaba/Modules/Concierge/Views/ConciergeViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ConciergeViewController: BaseViewController, ConciergeSubViewDelegate {
+class ConciergeViewController: BaseViewController, ConciergeSubViewDelegate, ImagePickerDelegate {
 
     @IBOutlet weak var scrollView: StackedScrollView!
 
     let conciergeSubView: ConciergeSubView = .fromNib()
+    private lazy var imagePicker = ImagePicker()
+
 
 
     override func viewDidLoad() {
@@ -20,16 +22,40 @@ class ConciergeViewController: BaseViewController, ConciergeSubViewDelegate {
         // Do any additional setup after loading the view.
 
         addconciergeSubView()
+        imagePicker.delegate = self
+
     }
 
     fileprivate func addconciergeSubView() {
         scrollView.stackView.addArrangedSubview(conciergeSubView)
         conciergeSubView.translatesAutoresizingMaskIntoConstraints = false
         conciergeSubView.heightAnchor.constraint(equalToConstant: 750).isActive = true
+        conciergeSubView.delegate = self
+    }
+
+    private func presentImagePicker(sourceType: UIImagePickerController.SourceType) {
+        imagePicker.present(parent: self, sourceType: sourceType)
     }
 
     // MARK:- ConciergeSubViewDelegate
     func didSelectUplaodConcierge() {
-        
+        imagePicker.photoGalleryAsscessRequest()
+    }
+
+    // MARK:- ImagePickerDelegate
+    func imagePickerDelegate(didSelect image: UIImage, delegatedForm: ImagePicker) {
+        conciergeSubView.bannerImageView.image = image
+        imagePicker.dismiss()
+    }
+
+    func imagePickerDelegate(didCancel delegatedForm: ImagePicker) { imagePicker.dismiss() }
+
+    func imagePickerDelegate(canUseGallery accessIsAllowed: Bool, delegatedForm: ImagePicker) {
+        if accessIsAllowed { presentImagePicker(sourceType: .photoLibrary) }
+    }
+
+    func imagePickerDelegate(canUseCamera accessIsAllowed: Bool, delegatedForm: ImagePicker) {
+        // works only on real device (crash on simulator)
+        if accessIsAllowed { presentImagePicker(sourceType: .camera) }
     }
 }
