5de5daebd5ceadd05b65685e6898ba89dceb4ab1
AhmedElsman <a.elsman@eightytech.com>
2019-10-23 22:20:48 +0200

    sort categories depends on position key

--- Tatayaba/Tatayaba/Modules/DashBoard/ViewModels/HomeViewModel.swift
+++ Tatayaba/Tatayaba/Modules/DashBoard/ViewModels/HomeViewModel.swift
@@ -58,7 +58,7 @@ class HomeViewModel {
                 guard let categoriesResult = response else { return }
                 guard let categories = categoriesResult.categories else { return }
 
-                self.categoriesList = categories//.filter({ $0.parentId == "0" })
+                self.categoriesList = categories.sorted(by: { Int($0.position) ?? 0 < Int($1.position) ?? 0 }) //.filter({ $0.parentId == "0" })
 
                 print(self.categoriesList)
 
