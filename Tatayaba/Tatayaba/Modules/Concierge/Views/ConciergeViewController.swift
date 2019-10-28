cb065fb0ab07f57ff98b231f2bbecb35888a66b3
AhmedElsman <a.elsman@eightytech.com>
2019-10-28 12:20:09 +0200

    solve problem of camera in concierge

--- Tatayaba/Tatayaba/Modules/Concierge/Views/ConciergeViewController.swift
+++ Tatayaba/Tatayaba/Modules/Concierge/Views/ConciergeViewController.swift
@@ -11,40 +11,40 @@ import UIKit
 class ConciergeViewController: BaseViewController, ConciergeSubViewDelegate, ImagePickerDelegate, CountryViewDelegate {
     
     
-
+    
     @IBOutlet weak var scrollView: StackedScrollView!
-
+    
     let conciergeSubView: ConciergeSubView = .fromNib()
     private lazy var imagePicker = ImagePicker()
     let viewModel = ConciergeViewModel()
-
-
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
         addconciergeSubView()
         imagePicker.delegate = self
         self.NavigationBarWithOutBackButton()
-
+        
     }
-
+    
     fileprivate func addconciergeSubView() {
         scrollView.stackView.addArrangedSubview(conciergeSubView)
         conciergeSubView.translatesAutoresizingMaskIntoConstraints = false
         conciergeSubView.heightAnchor.constraint(equalToConstant: 750).isActive = true
         conciergeSubView.delegate = self
     }
-
+    
     private func presentImagePicker(sourceType: UIImagePickerController.SourceType) {
         imagePicker.present(parent: self, sourceType: sourceType)
     }
-
+    
     // MARK:- ConciergeSubViewDelegate
     func didSelectUplaodConcierge() {
-
-       
+        
+        
         self.showAlert()
     }
     
@@ -61,35 +61,28 @@ class ConciergeViewController: BaseViewController, ConciergeSubViewDelegate, Ima
         self.present(alert, animated: true, completion: nil)
     }
     func getImage(fromSourceType sourceType: UIImagePickerController.SourceType) {
-   let imagePickerController = UIImagePickerController()
-        
-        if imagePickerController.sourceType == UIImagePickerController.SourceType.camera
-    {
-        imagePicker.cameraAsscessRequest()
-        
-        }else if imagePickerController.sourceType == UIImagePickerController.SourceType.photoLibrary{
-        imagePicker.photoGalleryAsscessRequest()
+        if sourceType == UIImagePickerController.SourceType.camera {
+            imagePicker.cameraAsscessRequest()
+            
+        } else if sourceType == UIImagePickerController.SourceType.photoLibrary{
+            imagePicker.photoGalleryAsscessRequest()
         }
-        
-        
-       
     }
-
+    
     func countrySelected(selectedCountry: Country) {
         conciergeSubView.countryButton.setTitle(selectedCountry.name, for: .normal)
         conciergeSubView.country = selectedCountry
     }
     
-    func didSelectCounty()
-    {
+    func didSelectCounty() {
         let controller = UIStoryboard(name: "Country", bundle: Bundle.main).instantiateViewController(withIdentifier: "CountryViewController") as! CountryViewController
         controller.delegate = self
         self.navigationController?.pushViewController(controller, animated: false)
     }
     
-
+    
     func didSelectSubmitConcierge(concierge: Concierge) {
-
+        
         showLoadingIndicator(to: self.view)
         viewModel.uploadConcierge(concierge: concierge) { result in
             self.hideLoadingIndicator(from: self.view)
@@ -97,28 +90,28 @@ class ConciergeViewController: BaseViewController, ConciergeSubViewDelegate, Ima
             case .success(let ConciergeResult):
                 print(ConciergeResult!)
                 self.showErrorAlerr(title: Constants.Concierge.uploaded, message: "Thanks for using concierge feature,\nWe will call you back withing 48 hours!".localized(), handler: { action in
-
+                    
                 })
-
+                
             case .failure(let error):
                 print("the error \(error)")
                 self.showErrorAlerr(title: Constants.Common.error, message: "Something went wrong while submitting your concierge!".localized(), handler: nil)
             }
         }
     }
-
+    
     // MARK:- ImagePickerDelegate
     func imagePickerDelegate(didSelect image: UIImage, delegatedForm: ImagePicker) {
         conciergeSubView.bannerImageView.image = image
         imagePicker.dismiss()
     }
-
+    
     func imagePickerDelegate(didCancel delegatedForm: ImagePicker) { imagePicker.dismiss() }
-
+    
     func imagePickerDelegate(canUseGallery accessIsAllowed: Bool, delegatedForm: ImagePicker) {
         if accessIsAllowed { presentImagePicker(sourceType: .photoLibrary) }
     }
-
+    
     func imagePickerDelegate(canUseCamera accessIsAllowed: Bool, delegatedForm: ImagePicker) {
         // works only on real device (crash on simulator)
         if accessIsAllowed { presentImagePicker(sourceType: .camera) }
