afdba71a6b17de6a182b2e914e6df16f85c3f782
Km77 <km.tatayab@gmail.com>
2019-11-24 09:07:02 +0300

    compressing concierge image to half

--- Tatayaba/Tatayaba/Utilities/Extensions/UIImage+Extensions.swift
+++ Tatayaba/Tatayaba/Utilities/Extensions/UIImage+Extensions.swift
@@ -10,7 +10,10 @@ import UIKit
 
 extension UIImage {
     func toBase64() -> String {
-        guard let imageData = self.pngData() else { return "" }
+        guard let compressData = self.jpegData(compressionQuality: 0.5) else { return "" } //max value is 1.0 and minimum is 0.0
+        guard let compressedImage = UIImage(data: compressData) else { return "" }
+        
+        guard let imageData = compressedImage.pngData() else { return "" }
         return imageData.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
     }
 }
